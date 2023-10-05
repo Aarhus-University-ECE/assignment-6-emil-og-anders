3045e2c555ac8419f1b81ea40efb145663375a7b
DammeyerEmil <143526887+DammeyerEmil@users.noreply.github.com>
2023-10-05 11:30:25 +0000

    cdty

--- max.c
+++ max.c
@@ -5,7 +5,18 @@
 int max(int *numbers, int size) {
   // Excercise 2
   // Implement your code below...
+    assert(size > 0);
 
+    int maxValue = numbers[0];
+    
+    for(int i = 0; i < size; i++){
+
+      if(maxValue < numbers[i]) {
+        maxValue = numbers[i];
+      }
+
+    }
+    
   return -1;
 }
 
